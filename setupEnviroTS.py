3ac0ac7eaa2ebdf76a4159ca6722e202d54a5bdc
mr-hespicable <leonhmcqueen@gmail.com>
2022-12-30 14:41:40 +0000

    Update setupEnviroTS.py and defaultTSCONFIG.txt

--- setupEnviroTS.py
+++ setupEnviroTS.py
@@ -5,17 +5,18 @@ path = input('Where would you like the project to be created?\n')
 name = input('What would you like the project to be called?\n')
 
 cdToPath = f'cd {path} &&'
-os.system(f'{cdToPath} mkdir {name}')
+os.system(f'{cdToPath} mkdir {name}') #creating directory
 dir = f'cd {path}/{name} &&'
-os.system(f'{dir} npm init -y && {dir} npm i typescript --save-dev && {dir} npm i @types/node && {dir} npx tsc && {dir} npx tsc --init')
+os.system(f'{dir} npm init -y && {dir} npm i typescript --save-dev && {dir} npm i @types/node && {dir} npx tsc && {dir} npx tsc --init') #setting up intial environment
 
 with open(f'defaultTSCONFIG.txt', 'r') as firstfile, open(f'{path}/{name}/tsconfig.json', 'a') as secondfile:
     for line in firstfile:
       secondfile.write(line)
   
-os.system(f'{dir} touch test.ts')
+os.system(f'{dir} touch test.ts') #creating test file
 with open(f'{path}/{name}/test.ts', 'w+') as g:
     g.write('console.log(\'<<<<ENVIRONMENT SETUP SUCCESSFUL>>>>\')')
+
 os.system('clear')
 os.system(f'{dir} npx tsc test.ts && node test.js')
 os.system(f'{dir} rm -rf test.ts && rm -rf test.js')
